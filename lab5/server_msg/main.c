73c3cd32505018923667718df707f13d882d5702
Tishuk <tishuk52@gmail.com>
2018-12-02 14:38:22 +0300

    queue_limit

--- lab5/server_msg/main.c
+++ lab5/server_msg/main.c
@@ -9,10 +9,12 @@
 //#include <sys/getloadavg.h>
 int main()
 {
-    key_t key = 1;
+    struct msqid_ds queue;
+    key_t key = 2;
     s_info * msg;
     int msgid;
     int err;
+
     msgid=msgget(key, IPC_CREAT|0666);
     if(msgid==-1){
     perror("can't get message queue");
@@ -29,18 +31,24 @@ return EXIT_FAILURE;
 perror("can't get uid");
 return EXIT_FAILURE;
 }
+msgctl(msgid,IPC_STAT,&queue);
+queue.msg_qbytes = sizeof(msg);
+msgctl(msgid,IPC_SET,&queue);
+
+
     msg->gid=getgid();
     if(msg->gid ==-1){
 perror("can't get gid");
 return EXIT_FAILURE;
 }
+
    while (1){
     msg->tm =time(NULL);
     getloadavg(msg->load, 3);
-    err=msgsnd(msgid, msg,sizeof(s_info),0 );
+    err=msgsnd(msgid, msg,sizeof(s_info),0);
 if(err==-1){perror("can't send message");}
 sleep(1);
-msgrcv(msgid, msg,sizeof(s_info),0,IPC_NOWAIT );
+//msgrcv(msgid, msg,sizeof(s_info),0,IPC_NOWAIT );
 
 }
     return EXIT_SUCCESS;
