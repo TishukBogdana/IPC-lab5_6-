8ecaa661b42ece761959ccf9610e67c905b3b267
Tishuk <tishuk52@gmail.com>
2018-12-13 11:54:03 +0300

    сыромятина, но аргументы доходят

--- lab6/server/main.c
+++ lab6/server/main.c
@@ -4,37 +4,46 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <errno.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 int main()
 {
-    int con_fd, res, i, len =0;
+    int con_fd, res, i, num, len =0;
     struct sockaddr_in serv_addr;
-    char * token;
-    char * buf = (char*) malloc(sizeof(int)) ;
+
+
+    char * buf = (char*) malloc(sizeof(char)*5);
     serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=htons(INADDR_ANY);
+    serv_addr.sin_addr.s_addr=INADDR_ANY;
     serv_addr.sin_port=htons(2222);
   int fd = socket(AF_INET, SOCK_STREAM,IPPROTO_TCP);
   if(fd==-1){perror("can't obtain socket");}
-  bind(fd , (struct sockaddr_in*)&serv_addr, sizeof(serv_addr));
+  if(bind(fd , (struct sockaddr_in*)&serv_addr, sizeof(serv_addr))){
+  perror("can't bind");
+  }
 
   while(1){
-     listen(fd, 10);
-     con_fd=accept(fd, (struct sockaddr_in*)NULL,NULL);
-     res = fork();
-     if(res==0){
-      read(con_fd, buf, 4);
-      len = atoi(buf);
-      buf=(char*)malloc(len);
-      read(con_fd, buf, len);
-      token = strtok(buf," ");
-      while(token){
-      send_data(token[i],con_fd);
-      }
-     }
+     if(listen(fd, 10)==-1){
+     perror("can't listen");};
+     con_fd=accept(fd, NULL,NULL);
+     if(con_fd==-1){perror("connect problem");}
 
-  }
+     res = 0;
+     if(res==0){
+      read(con_fd, buf, 5);
+      len = atoi(buf);
+      read(con_fd, buf, 5);
+      num = atoi(buf);
+      buf=(char*) malloc(len);
+      send_data(buf,con_fd);
+
+      }
+}
+
+
 
 
     return 0;
@@ -51,5 +60,8 @@ void send_data(char * dirname, int con_fd){
   }while(entry);
   closedir(dir);
  }
- ///todo: try with get_data
+ ///todo: try with get_data, error handle
 
+char ** delim(){
+   char * get,resv;
+}
